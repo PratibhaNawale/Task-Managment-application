9a7b86bedca490d83b8893c8c7611e0148cbf8b8
Pratibha Nawale <nawalepratibha28@gmail.com>
2024-12-04 10:33:24 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './component/navbar/Navbar';
+import TodoList from './pages/task-list/TodoList';
+import '../src/styles/TaskList.css';
+import Footer from './component/footer/Footer';
+import { Box } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import PageNotFound from './pages/not-found/PageNotFound';
+import '../src/styles/Theme.css';
+import AddSubTask from './component/sub-task/AddSubTask';
+import SignIn from './pages/login/SignIn';
+import SignUp from './pages/login/SignUp';
+
+function App() {
+  const [selectedFilter, setSelectedFilter] = useState('all');
+
+  return (
+    <Router>
+      <Box>
+        <ConditionalNavbarFooter selectedFilter={selectedFilter} setSelectedFilter={setSelectedFilter} />
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/all-task" element={<TodoList selectedFilter="all-task" />} />
+          <Route path="/pending" element={<TodoList selectedFilter="pending" />} />
+          <Route path="/completed" element={<TodoList selectedFilter="completed" />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Box>
+    </Router>
+  );
+}
+
+function ConditionalNavbarFooter({ selectedFilter, setSelectedFilter }) {
+  const location = useLocation();
+
+  const isAuthPage = location.pathname === '/signin' || location.pathname === '/signup';
+
+  return (
+    <>
+      {!isAuthPage && <Navbar selectedFilter={selectedFilter} onFilterChange={setSelectedFilter} />}
+      {!isAuthPage && <Footer />}
+    </>
+  );
+}
+
+export default App;
